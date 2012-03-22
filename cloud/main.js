ebd8e6b295d16c53f49dcb89981bfd106488329d
wei.li@feedhenry.com <wei.li@feedhenry.com>
2012-03-22 12:41:44 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -13,3 +13,22 @@ exports.getConfig = function(params, callback) {
   var cfg = require("config.js");
   return callback(null, {data: cfg.config});
 };
+
+
+exports.testWeb = function(params, callback) {
+  $fh.web({
+    url: "http://www.google.com",
+    method: "GET",
+    contentType: "text/html",
+    charset: "UTF-8",
+    period: 60000 //cache for 1 min
+  }, function(err, result) {
+    if (err) {
+      console.log("Error : " + err.message);
+    } else {
+      var data = result.body;
+      console.log("Response is " + data);
+    }
+    return callback(err, result);
+  })
+}
